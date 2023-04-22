c58d7bb28bd7ba3760bbb625862b024a2572a115
DimaSurdiy <dsurdiy@ukr.net>
2023-04-22 19:28:07 +0300

    add slider js

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+  effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
