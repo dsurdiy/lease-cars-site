15e55e62a81cbd148ba7899138c3fd5d35bf5cda
DimaSurdiy <dsurdiy@ukr.net>
2023-05-11 21:23:17 +0300

    add swiper.js file

---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
